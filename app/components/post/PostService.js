0d0e312ad5d6c604a9cca57e747c1a84bc8c96f0
Nguyen Duc Hao <haoxaomitom@gmail.com>
2024-10-29 19:25:46 +0700

    Complete comment, detailUser, report, share, doing Favorite

--- app/components/post/PostService.js
+++ app/components/post/PostService.js
@@ -1,34 +1,34 @@
-app.service('ItemService', function($http) {
-    this.getPosts = function(page, size) {
+app.service('ItemService', function ($http) {
+    this.getPosts = function (page, size) {
         return $http.get('http://localhost:8080/api/posts?page=' + page + '&size=' + size);
     };
 });
 
 // API for fetching location data
-app.service('LocationService', ['$http', function($http) {
-    this.getProvinces = function() {
+app.service('LocationService', ['$http', function ($http) {
+    this.getProvinces = function () {
         return $http.get('https://raw.githubusercontent.com/kenzouno1/DiaGioiHanhChinhVN/master/data.json');
     };
 }]);
 
 // API for fetching posts count by district and search
-app.service('PostService', ['$http', function($http) {
-    this.getPostsCountByDistrict = function() {
+app.service('PostService', ['$http', function ($http) {
+    this.getPostsCountByDistrict = function () {
         return $http.get('http://localhost:8080/api/posts/countByDistrict');
     };
 
-    this.searchPosts = function(searchTerm, selectedDistrictName, page) {
+    this.searchPosts = function (searchTerm, selectedDistrictName, page) {
         return $http({
             method: 'GET',
             url: 'http://localhost:8080/api/posts/search', // Ensure you include the full URL
-            params: { 
+            params: {
                 searchTerm: searchTerm,
                 district: selectedDistrictName,
                 page: page
             }
         });
-    }; 
-    this.searchPostsByVehicleType = function(vehicleType, page) {
+    };
+    this.searchPostsByVehicleType = function (vehicleType, page) {
         return $http({
             method: 'GET',
             url: 'http://localhost:8080/api/posts/searchVehicleType',
@@ -38,5 +38,17 @@ app.service('PostService', ['$http', function($http) {
             }
         });
     };
-             
+
+    // New method for sorting posts by price
+    this.sortPostsByPrice = function (sortOrder, page, size) {
+        return $http({
+            method: 'GET',
+            url: 'http://localhost:8080/api/posts/sort',
+            params: {
+                sort: sortOrder,
+                page: page,
+                size: size
+            }
+        });
+    };
 }]);
