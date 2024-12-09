1f02e1ca5b02d5001c5e2305dd725f39baf55102
Nguyen Duc Hao <haoxaomitom@gmail.com>
2024-12-09 14:56:28 +0700

    update connect to be

--- app/components/post/PostService.js
+++ app/components/post/PostService.js
@@ -1,6 +1,6 @@
 app.service('ItemService', function ($http) {
     this.getPosts = function (page, size) {
-        return $http.get('http://localhost:8080/api/posts/top?page=' + page + '&size=' + size);
+        return $http.get('https://doantotnghiepbe-production.up.railway.app/api/posts/top?page=' + page + '&size=' + size);
     };
 });
 
@@ -15,19 +15,19 @@ app.service('LocationService', ['$http', function ($http) {
 app.service('PostService', ['$http', function ($http) {
     // Fetch paginated posts
     this.getPosts = function (page, size) {
-        return $http.get('http://localhost:8080/api/posts/top');
+        return $http.get('https://doantotnghiepbe-production.up.railway.app/api/posts/top');
     };
 
     // Fetch posts count by district
     this.getPostsCountByDistrict = function () {
-        return $http.get('http://localhost:8080/api/posts/countByDistrict');
+        return $http.get('https://doantotnghiepbe-production.up.railway.app/api/posts/countByDistrict');
     };
 
     // Search posts by search term and district
     this.searchPosts = function (searchTerm, selectedDistrictName, page) {
         return $http({
             method: 'GET',
-            url: 'http://localhost:8080/api/posts/search',
+            url: 'https://doantotnghiepbe-production.up.railway.app/api/posts/search',
             params: {
                 searchTerm: searchTerm,
                 district: selectedDistrictName,
@@ -40,7 +40,7 @@ app.service('PostService', ['$http', function ($http) {
     this.searchPostsByVehicleType = function (vehicleType, page) {
         return $http({
             method: 'GET',
-            url: 'http://localhost:8080/api/posts/searchVehicleType',
+            url: 'https://doantotnghiepbe-production.up.railway.app/api/posts/searchVehicleType',
             params: {
                 vehicleType: vehicleType,
                 page: page
@@ -52,7 +52,7 @@ app.service('PostService', ['$http', function ($http) {
     this.sortPostsByPrice = function (sortOrder, page, size) {
         return $http({
             method: 'GET',
-            url: 'http://localhost:8080/api/posts/sort',
+            url: 'https://doantotnghiepbe-production.up.railway.app/api/posts/sort',
             params: {
                 sort: sortOrder,
                 page: page,
