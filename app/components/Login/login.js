563b2b1297be4f306a316da899e3572d3f4b979b
Nguyen Duc Hao <haoxaomitom@gmail.com>
2024-12-17 16:07:15 +0700

    fix bug after merge bao to hao

--- app/components/Login/login.js
+++ app/components/Login/login.js
@@ -1,9 +1,40 @@
-let app = angular.module('parkingApp', []);
-
 app.controller('LoginController', function ($scope, $location, $http, $window) {
     $scope.login = {
         username: '',
         password: ''
+    }
+
+    $scope.submitFormLogin = function () {
+        const data = {
+            username: $scope.login.username,
+            password: $scope.login.password
+        }
+        $http.post('http://localhost:8080/api/users/login', data)
+            .then(function (response) {
+                if (response.data.status) {
+
+                    const token = response.data.data.token;
+                    const userId = response.data.data.userId;
+                    localStorage.setItem('token', token);
+                    localStorage.setItem('username', $scope.login.username);
+                    localStorage.setItem('userId', userId);
+                    const redirectUrl = localStorage.getItem('redirectUrl');
+                    if (redirectUrl) {
+                        // Xóa URL đã lưu sau khi chuyển hướng
+                        localStorage.removeItem('redirectUrl');
+                        // Chuyển hướng đến trang trước đó
+                        $location.absUrl(redirectUrl);
+                    } else {
+                        // Mặc định 
+                        $window.location.href = '/';
+                    }
+                } else {
+                    $scope.message = response.data.message;
+                }
+            }, function (error) {
+                console.log(error);
+            }
+            )
     }
 
     $scope.submitFormRegister = function () {
